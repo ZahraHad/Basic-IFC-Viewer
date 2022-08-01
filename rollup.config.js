e0ef505471b0275eb257a3765063a6962f3a2551
Zahra <zahra.hadjou@gmail.com>
2022-08-01 17:40:00 +0200

    Final basic viewer

---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import resolve from "@rollup/plugin-node-resolve"
+
+export default {
+    input: "Model-viewer.js",
+    output: [
+        {
+            format: `esm`,
+            file: `./dist/bundle.js`
+        }
+    ],
+    plugins: [
+        resolve()
+    ]
+
+}
